336b426eec946654d26baec40eb46bf136e218e4
guyko <guyko91@gmail.com>
2022-04-30 21:56:38 +0900

    백준 11일차 (토요일)

--- src/baekjoon/step8/No1929.java
+++ src/baekjoon/step8/No1929.java
@@ -3,35 +3,38 @@ package baekjoon.step8;
 import java.io.*;
 
 public class No1929 {
+    public static boolean[] prime;
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String input = r.readLine();
 
-        int a = Integer.parseInt(input.split(" ")[0]);
-        int b = Integer.parseInt(input.split(" ")[1]);
+        int M = Integer.parseInt(input.split(" ")[0]);
+        int N = Integer.parseInt(input.split(" ")[1]);
 
-        for(int i = a; i <= b; i++) {
-            if(isPrime(i)) {
-                w.write(i + "\n");
-            }
+        prime = new boolean[N + 1];
+        get_prime();
+
+        for(int i = M; i <=N; i++) {
+            if(!prime[i]) w.write(i + "\n");
         }
+
         w.flush();
         w.close();
         r.close();
     }
 
-    public static boolean isPrime(int num) {
-        if(num == 1) {
-            return false;
-        }else {
-            for(int i = 2; i < num; i++) {
-                if(num%i == 0) {
-                    return false;
-                }
+    public static void get_prime() {
+        // true : 소수 아님, false : 소수
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i < Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
             }
-            return true;
         }
+
     }
 }
