02744ae8e8d6e81ff659417447a25c3c4309aee5
guyko <guyko91@gmail.com>
2022-05-14 21:30:09 +0900

    계수 정렬 추가.

---
+++ src/common/sorting/CountingSort.java
@@ -0,0 +1,44 @@
+package common.sorting;
+
+// 게수 정렬 (Counting Sort)
+public class CountingSort {
+    public static void main(String[] args) {
+
+        // 수열의 원소는 50개
+        int[] arr = new int[50];
+        // 수의 범위는 0 ~ 50
+        int[] countingArr = new int[51];
+        // 정렬 결과를 담을 배열.
+        int[] result = new int[50];
+
+        // arr에 0 ~ 50 사이의 수를 랜덤하게 세팅.
+        for(int i = 0; i < 50; i++) {
+            arr[i] = (int)(Math.random()*50);
+        }
+
+        // 정렬 시작.
+        for(int i = 0; i < arr.length; i++) {
+            // arr의 값을 index로 하는 counting 배열에 값을 증가 시켜준다.
+            countingArr[arr[i]]++;
+        }
+
+        // counting 배열에 누적 합 해주기. (index 1 부터)
+        for(int i = 1; i < countingArr.length; i++) {
+            countingArr[i] += countingArr[i - 1];
+        }
+
+        // 결과 배열에 세팅. (맨 뒤부터 수행)
+        for(int i = arr.length - 1; i >= 0; i--) {
+            // i 번째 원소를 인덱스로 하는 counting 배열을 1 감소시킨 뒤
+            // counting 배열의 값을 인덱스로 하여 resultㅔ value 값을 저장한다.
+            int value = arr[i];
+            countingArr[value]--;
+            result[countingArr[value]] = value;
+        }
+
+        for(int a : result) {
+            System.out.println(a);
+        }
+
+    }
+}
--- src/common/sorting/README.md
+++ src/common/sorting/README.md
@@ -37,5 +37,43 @@ public class Main {
         }
     }
 }
+```
 
+## 2. 계수정렬 (Counting Sort)
+* 데이터의 값이 몇번 나왔는지를 세주는 방식.
+* 주어진 배열의 값 범위가 작은 경우 빠른 속도를 갖는 정렬 알고리즘.
+* 시간 복잡도 O(n + k) (k = 배열의 최대값)
+* 입력값의 범위가 작을 때 높은 효율을 보인다.
+```java
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {5,3,1,2,4};
+        // 수의 범위 1 ~ 5
+        int[] countingArr = new int[5];
+        // 결과를 담을 배열 result
+        int[] result = new int[5];
+        
+        // counting 배열에 array의 값에 해당하는 index 공간에 수를 세어준다. (counting)
+        for(int i = 0; i < countingArr.length; i++) {
+            countingArr[array[i]]++;
+        }
+        
+        // counting 배열의 누적합을 구해준다. (index 1부터)
+        for(int i = 1; i < countingArr.length; i++) {
+            countingArr[i] += countingArr[i - 1];
+        }
+        
+        // result에 세팅 (array 의 뒤부터 수행)
+        for(int i = array.length - 1; i >= 0; i--) {
+            int value = array[i];
+            countingArr[value]--;
+            result[countingArr[value]] = value;
+        }
+        
+        // 정렬 결과 확인
+        for(int a : result) {
+            System.out.println(a);
+        }
+    }
+}
 ```
