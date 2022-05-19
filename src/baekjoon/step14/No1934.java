24046c3a662aa9f063a6a6d2634c1619e1d72dee
guyko91 <guyko91@gmail.com>
2022-05-19 12:52:50 +0900

    정수론 추가

---
+++ src/baekjoon/step14/No1934.java
@@ -0,0 +1,26 @@
+package baekjoon.step14;
+
+import java.util.Scanner;
+
+// 최소 공배수
+public class No1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println((a*b) / gcd(a,b));
+        }
+
+        sc.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if ( b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+}
